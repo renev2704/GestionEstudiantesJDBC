b1d8909cb38824ebd81dda7c129d782306371842
Alejandra Carolina Guillen Campos <alejandraxgcampos@gmail.com>
2026-09-03 13:46:57 -0600

    feat: registro de usuario y conexion DB

--- src/main/java/sv/edu/utec/modelo/Estudiante.java
+++ src/main/java/sv/edu/utec/modelo/Estudiante.java
@@ -1,4 +1,45 @@
 package sv.edu.utec.modelo;
 
+
 public class Estudiante {
+
+    private int id;
+    private String nombre;
+    private String carrera;
+    private double promedio;
+
+    public Estudiante(int id, String nombre, String carrera, double promedio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.promedio = promedio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
 }
